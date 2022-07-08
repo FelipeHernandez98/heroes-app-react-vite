8843c100648f8159e4c7b4b72f37b5e1bd7b0967
unknown <andresfelipehc@ufps.edu.co>
2022-07-08 13:42:29 -0500

    implementando rutas privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,15 +1,21 @@
 import { Route, Routes } from "react-router-dom"
 import { LoginPage } from "../auth"
 import { HeroesRoutes } from "../heroes"
+import { PrivateRoute } from "./PrivateRoute"
 
 export const AppRouter = () => {
   return (
     <>
-        
-        <Routes>
-            <Route path="login" element={<LoginPage />} />
-            <Route path="/*" element={<HeroesRoutes />} />
-        </Routes>
+
+      <Routes>
+        <Route path="login" element={<LoginPage />} />
+
+        <Route path="/*" element={
+          <PrivateRoute>
+            <HeroesRoutes />
+          </PrivateRoute>} />
+        {/* <Route path="/*" element={<HeroesRoutes />} /> */}
+      </Routes>
     </>
   )
 }
